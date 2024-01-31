39ccb82141e17f68f7c73a19d72c3a13ab5fc608
Your Name <nguyphuquy1@gmail.com>
2024-01-31 18:08:46 +0700

    first commit

---
+++ server/prisma/answers.ts
@@ -0,0 +1,199 @@
+export default [
+    {
+        content: "Trưng Tam",
+        questionId: 1
+        , is_answer: false
+    },
+    {
+        content: "Trưng Nhị",
+        questionId: 1
+        , is_answer: false
+    },
+    {
+        content: "Lý Bị",
+        questionId: 1
+        , is_answer: false
+    },
+    {
+        content: "Trưng Trắc",
+        questionId: 1
+        , is_answer: true
+    },
+    {
+        content: "Lễ hội Chọi trâu",
+        questionId: 2
+        , is_answer:   false},
+    {
+        content: "Lễ hội Cầu Ngư",
+        questionId: 2
+        , is_answer:  false },
+    {
+        content: "Lễ hội Trống Đồng",
+        questionId: 2
+        , is_answer: false  },
+    {
+        content: "Lễ hội Xuân",
+        questionId: 2
+        , is_answer:  true },
+    {
+        content: "Lý Thái Tổ",
+        questionId: 3
+        , is_answer:  true },
+    {
+        content: "Trần Thái Tông",
+        questionId: 3
+        , is_answer: false  },
+    {
+        content: "Lê Lợi",
+        questionId: 3
+        , is_answer: false  },
+    {
+        content: "Ngô Quyền",
+        questionId: 3
+        , is_answer: false  },
+    {
+        content: "1975",
+        questionId: 4
+        , is_answer: false  },
+    {
+        content: "1955",
+        questionId: 4
+        , is_answer: false  },
+    {
+        content: "1968",
+        questionId: 4
+        , is_answer: false  },
+    {
+        content: "1954",
+        questionId: 4
+        , is_answer: true  },
+    {
+        content: "Nguyễn Huệ",
+        questionId: 5
+        , is_answer: false  },
+    {
+        content: "Lê Lợi",
+        questionId: 5
+        , is_answer: true  },
+    {
+        content: "Trần Hưng Đạo",
+        questionId: 5
+        , is_answer: false  },
+    {
+        content: "Lê Hoàn",
+        questionId: 5
+        , is_answer: false  },
+    {
+        content: "Lê Lợi",
+        questionId: 6
+        , is_answer: false  },
+    {
+        content: "Lê Hoàn",
+        questionId: 6
+        , is_answer: false  },
+    {
+        content: "Trịnh Căn",
+        questionId: 6
+        , is_answer: true  },
+    {
+        content: "Nguyễn Huệ",
+        questionId: 6
+        , is_answer: false  },
+    {
+        content: "<h2>",
+        questionId: 7
+        , is_answer: true  },
+    {
+        content: "<p>",
+        questionId: 7
+        , is_answer: false  },
+    {
+        content: "<h1>",
+        questionId: 7
+        , is_answer: false  },
+    {
+        content: "<div>",
+        questionId: 7
+        , is_answer: false  },
+    {
+        content: "background-image",
+        questionId: 8
+        , is_answer: false  },
+    {
+        content: "color",
+        questionId: 8
+        , is_answer: false  },
+    {
+        content: "background-color",
+        questionId: 8
+        , is_answer: true  },
+    {
+        content: "font-size",
+        questionId: 8
+        , is_answer: false  },
+    {
+        content: "<ul>",
+        questionId: 9
+        , is_answer: false  },
+    {
+        content: "<ol>",
+        questionId: 9
+        , is_answer: false  },
+    {
+        content: "<li>",
+        questionId: 9
+        , is_answer: false  },
+    {
+        content: "<ul> và <li>",
+        questionId: 9
+        , is_answer: true  },
+    {
+        content: "<image>",
+        questionId: 10
+        , is_answer: false  },
+    {
+        content: "<picture>",
+        questionId: 10
+        , is_answer: false  },
+    {
+        content: "<img>",
+        questionId: 10
+        , is_answer: true  },
+    {
+        content: "<src>",
+        questionId: 10
+        , is_answer: false  },
+    {
+        content: ".box",
+        questionId: 11
+        , is_answer: true  },
+    {
+        content: "#box",
+        questionId: 11
+        , is_answer: false  },
+    {
+        content: ".box",
+        questionId: 11
+        , is_answer: false  },
+    {
+        content: "div.box",
+        questionId: 11
+        , is_answer: false  },
+    {
+        content: "border-radius",
+        questionId: 12
+        , is_answer: false  },
+    {
+        content: "background-color",
+        questionId: 12
+        , is_answer: false  },
+    {
+        content: "border",
+        questionId: 12
+        , is_answer: true  },
+    {
+        content: "margin",
+        questionId: 12
+        , is_answer: false  },
+
+]
---
+++ server/prisma/questions.ts
@@ -0,0 +1,54 @@
+export default [
+    {
+        categoryId: 1,
+        content: "Ai là người đã lãnh đạo cuộc khởi nghĩa Hai Bà Trưng năm 40 sau Công nguyên?",
+        level: 1
+    },
+    {
+        categoryId: 1,
+        content: "Đền Hùng là nơi tổ chức lễ hội nào??",
+        level: 1
+    }, {
+        categoryId: 1,
+        content: "Ai là người đã thành lập Đại Việt và trở thành vị hoàng đế đầu tiên?",
+        level: 2
+    }, {
+        categoryId: 1,
+        content: "Trận Chiến Điện Biên Phủ diễn ra vào năm nào?",
+        level: 2
+    }, {
+        categoryId: 1,
+        content: "Cuộc Khởi nghĩa Lam Sơn do ai lãnh đạo?",
+        level: 3
+    }, {
+        categoryId: 1,
+        content: "Ai là người đã lãnh đạo cuộc khởi nghĩa Nam Kỳ năm 1673?",
+        level: 3
+    },
+    {
+        categoryId: 2,
+        content: "Để tạo một tiêu đề trong HTML, bạn sử dụng thẻ nào?",
+        level: 1
+    },
+    {
+        categoryId: 2,
+        content: "Thuộc tính CSS nào dùng để đặt màu nền cho một phần tử HTML?",
+        level: 1
+    }, {
+        categoryId: 2,
+        content: "Để tạo một danh sách không có thứ tự trong HTML, bạn sử dụng thẻ nào?",
+        level: 2
+    }, {
+        categoryId: 2,
+        content: "Để thêm một hình ảnh vào trang web, bạn sử dụng thẻ nào trong HTML?",
+        level: 2
+    }, {
+        categoryId: 2,
+        content: "Để chọn tất cả các phần tử <div> có class là 'box', bạn sử dụng CSS selector nào?",
+        level: 3
+    }, {
+        categoryId: 2,
+        content: "Để thêm một đường viền xung quanh một phần tử HTML, bạn sử dụng thuộc tính CSS nào?",
+        level: 3
+    }
+]
